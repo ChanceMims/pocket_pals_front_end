434330262729c0f7c1924435e6ebd813fe8eecd8
Chance Mims <chance@Chances-MacBook-Air.local>
2020-04-23 16:50:07 -0500

    fixed some drag issues with cards and zones, and added effects to zones for activation. Working on dropping in activation zone

--- pocket-pals-front-end/src/containers/Battlefield.js
+++ pocket-pals-front-end/src/containers/Battlefield.js
@@ -3,97 +3,51 @@ import {Grid} from 'semantic-ui-react'
 import ActionLog from '../components/ActionLog'
 import InactivePals from '../components/InactivePals'
 import ActivePals from '../components/ActivePals'
+
  
 class Battlefield extends Component{
 
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state={
             messages: [],
-            myPals: [],
+            myPals: [...props.pals],
             turn: 'player',
-            selectedPal: ''
+            selectedPal: '',
+            botPals: []
         }
     }
 
-    componentDidMount(){
-        this.assignPalsStatus()
+    componentDidMount() {
+        this.getBotPals(this.state.myPals.length);
+        this.assignPalsStatus();
     }
-
 
     getBotPals = (size) => {
-        const botPals = [{
-            name: 'charmander',
-            attck: 64,
-            def: 58,
-            hp: 58,
-            img_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
-            element: 'fire',
-            status: 'inactive'
-        },
-        {
-            name: 'beedrill',
-            attck: 90,
-            def: 40,
-            hp: 65,
-            img_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png',
-            element: 'bug',
-            status: 'inactive'
-        },
-        {
-            name: 'jigglypuff',
-            attck: 45,
-            def: 20,
-            hp: 115,
-            img_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png',
-            element: 'normal',
-            status: 'inactive'
+        fetch("http://localhost:3000/pocket_pals")
+            .then(resp => resp.json())
+            .then(json => {
+                this.sample(json, size)
+            })
+    }
 
-        },
-        {
-                name: 'tentacruel',
-                attck: 70,
-                def: 65,
-                hp: 70,
-                img_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/73.png',
-                element: 'water',
-                status: 'inactive'
-        },
-        {
-                name: 'onix',
-                attck: 45,
-                def: 160,
-                hp: 35,
-                img_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/95.png',
-                element: 'rock',
-                status: 'inactive'
-        },
-        {
-                name: 'tangela',
-                attck: 55,
-                def: 115,
-                hp: 65,
-                img_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/114.png',
-                element: 'grass',
-                status: 'inactive'
+    sample = (pals, size) => {
+        const palsArray = [];
+
+        for (let counter = 0; counter < size; counter++){
+            const statPal = pals[Math.floor(100 * Math.random(0, pals.size))]
+            statPal.status = "inactive";
+            palsArray.push(statPal)
         }
-    ]
-
-    return botPals.slice(0, size)
+        this.setState({
+                    botPals: palsArray
+        })
     }
 
     assignPalsStatus = () => {
-        //console.log(this.props)
-        const palArray = [];
-        // this.props.pals.map(pal => {
-        //     pal.status = 'inactive'
-        //     palArray.push(pal)
-        //     console.log(pal)
-            
-        // })
-        this.setState({
-            myPals: palArray
-        })
+        this.state.myPals.map(pal => {
+            pal.status = 'inactive'
+        })     
     }
 
     handleDrag = (pal) => {
@@ -104,16 +58,9 @@ class Battlefield extends Component{
 
     handleActivate = () => {
 
-        const inactivePals = this.state.myPals.filter(pal => (
-            pal.name !== this.state.selectedPal.name
-        ))
-        const updatedPal = this.setState.selectedPal;
-        updatedPal.status = 'active';
+        this.state.selectedPal.status = 'active';
+        console.log('activate')
         this.setState({
-            selectedPal: updatedPal
-        })
-        this.setState({
-            myPals: [...inactivePals, this.state.selectedPal],
             messages: [...this.state.messages, `${this.state.selectedPal.name} has been activated`]
         })
 
@@ -127,25 +74,22 @@ class Battlefield extends Component{
                         <ActionLog messages={this.state.messages} />
                     </Grid.Column>
                     <Grid.Column width={10}>
-                        <InactivePals handleInactiveDrag={props => console.log(props)} pals={this.getBotPals(this.props.pals.length).filter(pal => (pal.status === 'inactive' ))}/>
+                        <InactivePals id='enemy-inactive' handleDrag={this.handleDrag} pals={this.state.botPals.filter(pal => (pal.status === 'inactive' ))}/>
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row>
-                    <Grid.Column centered >
-        
-                            <ActivePals pals={this.getBotPals(this.props.pals.length).filter(pal => (pal.status === 'active'))}/>
-                        
-                        
+                    <Grid.Column  >
+                            <ActivePals id='enemy-active' pals={this.state.botPals.filter(pal => (pal.status === 'active'))}/>
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row>
-                    <Grid.Column centered>
-                        <ActivePals handleActivate={this.handleActivate} handleDrag={this.handleDrag} pals={this.state.myPals.filter(pal => (pal.status === 'active' ))}/>
+                    <Grid.Column >
+                        <ActivePals id='user-active' handleActivate={this.handleActivate} handleDrag={this.handleDrag} pals={this.state.myPals.filter(pal => (pal.status === 'active' ))}/>
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row>
-                    <Grid.Column centered>
-                        <InactivePals handleDrag={this.handleDrag} pals={this.state.myPals.filter(pal => (pal.status === 'inactive'))}/>
+                    <Grid.Column >
+                        <InactivePals id='user-inactive' handleDrag={this.handleDrag} pals={this.state.myPals.filter(pal => (pal.status === 'inactive'))}/>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
